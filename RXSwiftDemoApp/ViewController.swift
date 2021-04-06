cb3303bbe9bd580958e3c00f75a26365526c5719
Waheed Afolabi <waheed.freelancer16@gmail.com>
2021-04-06 18:35:19 +0100

    Initial Commit

---
+++ RXSwiftDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RXSwiftDemoApp
+//
+//  Created by waheedCodes on 06/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
