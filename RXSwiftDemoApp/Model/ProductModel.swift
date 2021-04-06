2c8d5081e105390f3cb43d87ea29c6bc98ef6bcb
Waheed Afolabi <waheed.freelancer16@gmail.com>
2021-04-06 18:50:41 +0100

    add product model

---
+++ RXSwiftDemoApp/Model/ProductModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProductModel.swift
+//  RXSwiftDemoApp
+//
+//  Created by waheedCodes on 06/04/2021.
+//
+
+import Foundation
+
+struct ProductModel {
+    let imageName: String
+    let title: String
+}
--- RXSwiftDemoApp/ProductsViewController.swift
+++ RXSwiftDemoApp/ProductsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ProductsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
